16774d4c461e1e05ba746ec214202751dcc3662b
Sayan Naskar <47991790+sayann002@users.noreply.github.com>
2021-03-23 02:40:15 +0530

    Modifying Files

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -3,7 +3,7 @@ import React from "react";
 const LocationInfoBox = ({ info }) => {
   return (
     <div className="location-info">
-      <h2>Event Loaction Info</h2>
+      <h2>Event Location Info</h2>
       <ul>
         <li>
           ID: <strong>{info.id}</strong>
